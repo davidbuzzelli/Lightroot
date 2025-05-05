1231b2195e4379ee76ea1bf2004e0dbbf72893ce
David Buzzelli <139834225+davidbuzzelli@users.noreply.github.com>
2025-05-05 14:55:19 -0400

    Create interactive_nomad.py

---
+++ examples/interactive_nomad.py
@@ -0,0 +1,11 @@
+# examples/interactive_nomad.py
+
+"""
+This file runs the LightRoot CLI Adapter.
+Start here to interact with a basic Nomad + Eidos instance.
+"""
+
+from adapters.cli import start_cli
+
+if __name__ == "__main__":
+    start_cli()
