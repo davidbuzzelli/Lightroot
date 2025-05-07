ca0343500b1fd4868e9df5f35d4657f4066f5cb8
Vikash Pathak <vikash@sellcx.com>
2025-05-07 21:29:00 +0530

    Removed open ai key.

--- run_lightroot.ps1
+++ run_lightroot.ps1
@@ -1,6 +1,3 @@
-# Set environment variables
-$env:OPENAI_API_KEY = "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"  # Insert your actual key here
-
 # Set PYTHONPATH to current folder
 $env:PYTHONPATH = (Get-Location).Path
 
